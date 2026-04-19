1e488267f23a2c8ff7c200b119efb9894caa1664
Lucas Guima <lucasguima.felipe@gmail.com>
2026-04-19 20:56:18 -0300

    chore(analytics): set Google Analytics tracking ID

--- src/site.config.mjs
+++ src/site.config.mjs
@@ -38,6 +38,6 @@ export default {
     numberOfLatestPostsOnHomePage: 6, // Number of latest posts on homepage
     numberOfBlogPostsPerPage: 8, // Number of posts per paginated blog
 
-    gTag: "" // Google Analytics tracking ID
+    gTag: "G-X8970LK9KF" // Google Analytics tracking ID
 }
 
