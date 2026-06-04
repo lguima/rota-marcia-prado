1889bb64d4ce5a558dc5f49e0980ee59f4d74628
Lucas Guima <lucasguima.felipe@gmail.com>
2026-06-04 19:14:29 -0300

    feat(i18n): configure internationalization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import { unified } from '@astrojs/markdown-remark';
+import node from '@astrojs/node';
 import tailwindcss from "@tailwindcss/vite";
 import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
+import { unified } from '@astrojs/markdown-remark';
+
 import remarkYoutubeEmbed from './src/plugins/remarkYoutubeEmbed.js'
 import remarkVimeoEmbed from './src/plugins/remarkVimeoEmbed.js'
 import remarkWistiaEmbed from './src/plugins/remarkWistiaEmbed.js'
@@ -13,15 +16,31 @@ import remarkGistEmbed from './src/plugins/remarkGistEmbed.js'
 import remarkAscinemaEmbed from './src/plugins/remarkAscinemaEmbed.js';
 import remarkSoundcloudEmbed from './src/plugins/remarkSoundcloudEmbed.js';
 
-import sitemap from '@astrojs/sitemap';
-
 // https://astro.build/config
 export default defineConfig({
     site: 'https://rotamarciaprado.org',
+    output: "server",
+    adapter: node({
+      mode: 'standalone',
+    }),
     vite: {
         plugins: [tailwindcss()]
     },
     integrations: [partytown({ config: { forward: ['dataLayer.push', 'gtag'] } }), sitemap()],
+    i18n: {
+      locales: ["pt-BR", {
+        path: "es",
+        codes: ["es", "es-ES", "es-UY", "es-AR", "es-CL", "es-PY", "es-BO", "es-PE", "es-EC", "es-CO", "es-VE"],
+      },  {
+        path: "en",
+        codes: ["en", "en-US", "en-GB", "en-CA", "en-AU"],
+      }],
+      defaultLocale: "pt-BR",
+      fallback: {
+        es: "pt-BR",
+        en: "pt-BR",
+      },
+    },
     image: {
       remotePatterns: [{ protocol: "https" }],
       domains: ["res.cloudinary.com"],
