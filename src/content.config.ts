5b671b189c2fe8ccd00c618944d2a1128c1298c3
Lucas Guima <lucasguima.felipe@gmail.com>
2026-06-06 11:09:55 -0300

    chore(content.config): update indentation

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,20 +4,20 @@ import { glob } from 'astro/loaders';
 import SiteOptions from "./site.config.mjs"
 
 const articleCollection = defineCollection({
-    loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/article" }),
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        pubDate: z.date(),
-        author: z.string().default(SiteOptions.defaultAuthorName),
-        category: z.string(),
-        tags: z.array(z.string()),
-        featured: z.boolean().default(false),
-        thumb: z.string().optional(),
-        large: z.string().optional(),
-    }),
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/article" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    author: z.string().default(SiteOptions.defaultAuthorName),
+    category: z.string(),
+    tags: z.array(z.string()),
+    featured: z.boolean().default(false),
+    thumb: z.string().optional(),
+    large: z.string().optional(),
+  }),
 });
 
 export const collections = {
-    article: articleCollection,
+  article: articleCollection,
 };
