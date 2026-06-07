1bd1d11670a437a09fca4b5575ff88aeb7b1f65e
Lucas Guima <lucasguima.felipe@gmail.com>
2026-06-07 11:25:02 -0300

    chore(config): split site config to config folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,17 @@ import tailwindcss from "@tailwindcss/vite";
 import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
 import { unified } from '@astrojs/markdown-remark';
+import { loadEnv } from "vite";
 
 import remarkYouTubeEmbed from './src/plugins/remarkYouTubeEmbed.js'
 import remarkGoogleMapsEmbed from './src/plugins/remarkGoogleMapsEmbed.js'
 import remarkIframeEmbed from './src/plugins/remarkIframeEmbed.js'
 import remarkPdfEmbed from './src/plugins/remarkPdfEmbed.js'
 
+const { SITE_URL } = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), "");
+
 export default defineConfig({
-  site: 'https://rotamarciaprado.org',
+  site: SITE_URL ?? "https://rotamarciaprado.org",
   vite: {
     plugins: [tailwindcss()],
   },
---
+++ src/config/site.mjs
@@ -0,0 +1,3 @@
+export default {
+  url: import.meta.env.SITE_URL ?? "https://rotamarciaprado.org",
+}
