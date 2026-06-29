4ae47f84f30027b59cba70d194cdf0938ca61e17
Lucas Guima <lucasguima.felipe@gmail.com>
2026-06-28 21:48:24 -0300

    feat(config/site): add site config

--- src/config/site.mjs
+++ src/config/site.mjs
@@ -1,3 +1,10 @@
+/**
+ * Site
+ */
 export default {
-  url: import.meta.env.SITE_URL ?? "https://rotamarciaprado.org",
+  name: "Rota Márcia Prado",
+  metadata: {
+    alternateName: "Rota cicloturística Márcia Prado",
+    description: "Guia de cicloturismo da Rota Márcia Prado, com informações sobre o percurso, pontos de interesse, dicas de viagem e muito mais.",
+  },
 }
