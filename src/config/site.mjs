6396442ac7d85b11d7a21606161327474e4dcfb7
Lucas Guima <lucasguima.felipe@gmail.com>
2026-07-09 21:39:25 -0300

    feat(config/site): add subTitle

--- src/config/site.mjs
+++ src/config/site.mjs
@@ -3,6 +3,7 @@
  */
 export default {
   name: "Rota Márcia Prado",
+  subTitle: "Rota cicloturística que liga a Região Metropolitana de São Paulo a Santos pela Serra do Mar, conhecida como a tradicional descida de Santos",
   metadata: {
     alternateName: "Rota cicloturística Márcia Prado",
     description: "Guia de cicloturismo da Rota Márcia Prado, com informações sobre o percurso, pontos de interesse, dicas de viagem e muito mais.",
