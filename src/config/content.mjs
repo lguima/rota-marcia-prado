0bdfe8ce765655ea227d86bbb6aed63d2a6e877f
Lucas Guima <lucasguima.felipe@gmail.com>
2026-07-25 09:32:23 -0300

    feat(config/content): add authors pagination

--- src/config/content.mjs
+++ src/config/content.mjs
@@ -8,4 +8,10 @@ export default {
       rangeDelta: 1, // Number of pages to show around the current page
     },
   },
+  authors: {
+    pagination: {
+      pageSize: 6, // Number of posts per page
+      rangeDelta: 1, // Number of pages to show around the current page
+    },
+  },
 }
