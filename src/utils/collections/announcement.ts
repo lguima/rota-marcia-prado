142f18e9005e2af8d4f25069e86eb08d553c7deb
Lucas Guima <lucasguima.felipe@gmail.com>
2026-07-29 19:09:51 -0300

    feat(utils/collections/announcement): create utils for announcement collection

---
+++ src/utils/collections/announcement.ts
@@ -0,0 +1,12 @@
+import { getCollection } from 'astro:content'
+
+export async function getPublishedAnnouncements() {
+  const now = new Date();
+
+  return (await getCollection('announcement', ({ data }) => {
+    return now >= new Date(data.publishStartDate) && now <= new Date(data.publishEndDate);
+  }))
+  .sort((a, b) =>
+    new Date(b.data.publishStartDate).getTime() - new Date(a.data.publishStartDate).getTime()
+  )
+}
