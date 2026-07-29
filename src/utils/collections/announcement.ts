8602e62b979ef81b92c412e5bb36cf98f7a11e0c
Lucas Guima <lucasguima.felipe@gmail.com>
2026-07-29 19:47:59 -0300

    feat(utils/collections/announcement): filter announcements by active property

--- src/utils/collections/announcement.ts
+++ src/utils/collections/announcement.ts
@@ -4,7 +4,7 @@ export async function getPublishedAnnouncements() {
   const now = new Date();
 
   return (await getCollection('announcement', ({ data }) => {
-    return now >= new Date(data.publishStartDate) && now <= new Date(data.publishEndDate);
+    return data.active && now >= new Date(data.publishStartDate) && now <= new Date(data.publishEndDate);
   }))
   .sort((a, b) =>
     new Date(b.data.publishStartDate).getTime() - new Date(a.data.publishStartDate).getTime()
