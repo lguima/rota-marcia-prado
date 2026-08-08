f46d392020817541113d043e43c369e3201b9a46
Lucas Guima <lucasguima.felipe@gmail.com>
2026-08-08 20:54:14 -0300

    refactor(utils/collections/alert): split alerts into priority and secondary

--- src/utils/collections/alert.ts
+++ src/utils/collections/alert.ts
@@ -1,16 +1,28 @@
-import { getCollection } from 'astro:content'
+import { getCollection } from 'astro:content';
 
-export async function getActiveAlerts() {
-  const now = new Date()
+async function getActiveAlertsBySeverity(severities: number[]) {
+  const now = new Date();
 
-  return (
-    await getCollection('alert', ({ data }) => {
-      return data.active && new Date(data.publishDate) <= now
-    })
-  ).sort((a, b) => {
+  const alerts = await getCollection('alert', ({ data }) => {
+    const isCorrectSeverity = severities.includes(data.severity);
+    const isPublished = now >= new Date(data.publishDate);
+    const notExpired = !data.expirationDate || now <= new Date(data.expirationDate);
+
+    return isCorrectSeverity && data.active && isPublished && notExpired;
+  });
+
+  return alerts.toSorted((a, b) => {
     if (a.data.severity !== b.data.severity) {
-      return a.data.severity - b.data.severity
+      return a.data.severity - b.data.severity;
     }
-    return b.data.publishDate.valueOf() - a.data.publishDate.valueOf()
-  })
+    return new Date(b.data.publishDate).getTime() - new Date(a.data.publishDate).getTime();
+  });
+}
+
+export async function getActivePriorityAlerts() {
+  return getActiveAlertsBySeverity([1, 2]);
+}
+
+export async function getActiveSecondaryAlerts() {
+  return getActiveAlertsBySeverity([3, 4]);
 }
