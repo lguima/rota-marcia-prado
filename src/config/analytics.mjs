45ae679dbd57c22955568a584a550e74f257d977
Lucas Guima <lucasguima.felipe@gmail.com>
2026-08-09 20:33:26 -0300

    feat(config/analytics): add google analytics mode

--- src/config/analytics.mjs
+++ src/config/analytics.mjs
@@ -4,6 +4,7 @@
 export default {
   googleTag: {
     enabled: import.meta.env.ANALYTICS_GOOGLE_TAG_ENABLED !== "false",
-    measurementId: import.meta.env.ANALYTICS_GOOGLE_TAG_MEASUREMENT_ID ?? "G-X8970LK9KF"
+    measurementId: import.meta.env.ANALYTICS_GOOGLE_TAG_MEASUREMENT_ID ?? "G-X8970LK9KF",
+    mode: import.meta.env.ANALYTICS_GOOGLE_TAG_MODE ?? "anonymous",
   },
 }
