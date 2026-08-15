ddd8e1e1c250e5aafcda968f0e68034268fec370
Lucas Guima <lucasguima.felipe@gmail.com>
2026-08-15 09:17:15 -0300

    feat(utils/collections/structuredData): create function for website schema

--- src/utils/structuredData.ts
+++ src/utils/structuredData.ts
@@ -1,8 +1,22 @@
 import type { CollectionEntry } from 'astro:content'
+import site from '@config/site.mjs'
+
+const context = 'https://schema.org'
+
+export function getWebsiteSchema(siteUrl: URL | undefined) {
+  return {
+    '@context': context,
+    '@type': 'WebSite',
+    name: site.name,
+    alternateName: [site.metadata.alternateName],
+    description: site.metadata.description,
+    url: siteUrl,
+  }
+}
 
 export function getFaqSchema(faqs: CollectionEntry<'faq'>[]) {
   return {
-    '@context': 'https://schema.org',
+    '@context': context,
     '@type': 'FAQPage',
     mainEntity: faqs.map((faq) => ({
       '@type': 'Question',
