57ec133385656b3ed486eae5091c86b662ae62b4
Lucas Guima <lucasguima.felipe@gmail.com>
2026-08-15 14:57:36 -0300

    feat(scripts/cookieconsent-config): update script to add funcionality and marketing categories mention

--- public/scripts/cookieconsent-config.js
+++ public/scripts/cookieconsent-config.js
@@ -1,71 +1,77 @@
 import 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.1.0/dist/cookieconsent.umd.js';
 
-// Enable dark mode
-document.documentElement.classList.add('cc--darkmode');
-
 CookieConsent.run({
-    guiOptions: {
+  guiOptions: {
+    consentModal: {
+      layout: "box inline",
+      position: "bottom left",
+      equalWeightButtons: true,
+      flipButtons: false
+    },
+    preferencesModal: {
+      layout: "box",
+      position: "right",
+      equalWeightButtons: true,
+      flipButtons: false
+    }
+  },
+  categories: {
+    necessary: {
+      readOnly: true
+    },
+    functionality: {},
+    analytics: {},
+    marketing: {},
+  },
+  language: {
+    default: "pt-br",
+    autoDetect: "browser",
+    translations: {
+      'pt-br': {
         consentModal: {
-            layout: "box",
-            position: "bottom left",
-            equalWeightButtons: true,
-            flipButtons: false
+          title: "Preferências de Consentimento",
+          description: "Cookies são usados para aprimorar sua navegação no site.",
+          acceptAllBtn: "Aceitar tudo",
+          acceptNecessaryBtn: "Rejeitar tudo",
+          showPreferencesBtn: "Gerenciar preferêcias",
+          footer: "<a href=\"/politica-de-privacidade/\">Política de Privacidade</a>"
         },
         preferencesModal: {
-            layout: "box",
-            position: "right",
-            equalWeightButtons: true,
-            flipButtons: false
+          title: "Preferências de Consentimento",
+          acceptAllBtn: "Aceitar tudo",
+          acceptNecessaryBtn: "Rejeitar tudo",
+          savePreferencesBtn: "Salvar preferências",
+          closeIconLabel: "Fechar diálogo",
+          serviceCounterLabel: "Service|Services",
+          sections: [
+            {
+              title: "Uso de cookies",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            },
+            {
+              title: "Cookies estritamente necessários <span class=\"pm__badge\">Sempre habilitados</span>",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+              linkedCategory: "necessary"
+            },
+            {
+              title: 'Functionality Cookies',
+              description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+              linkedCategory: 'functionality',
+            },
+            {
+              title: "Analytics Cookies",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+              linkedCategory: "analytics"
+            },
+            {
+              title: "Advertisement Cookies",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+              linkedCategory: "marketing"
+            },
+          ]
         }
-    },
-    categories: {
-        necessary: {
-            readOnly: true
-        },
-        analytics: {}
-    },
-    language: {
-        default: "en",
-        autoDetect: "browser",
-        translations: {
-            en: {
-                consentModal: {
-                    title: "Hello traveller, it's cookie time!",
-                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
-                    acceptAllBtn: "Accept all",
-                    acceptNecessaryBtn: "Reject all",
-                    showPreferencesBtn: "Manage preferences",
-                    footer: "<a href=\"#link\">Privacy Policy</a>\n<a href=\"#link\">Terms and conditions</a>"
-                },
-                preferencesModal: {
-                    title: "Consent Preferences Center",
-                    acceptAllBtn: "Accept all",
-                    acceptNecessaryBtn: "Reject all",
-                    savePreferencesBtn: "Save preferences",
-                    closeIconLabel: "Close modal",
-                    serviceCounterLabel: "Service|Services",
-                    sections: [
-                        {
-                            title: "Cookie Usage",
-                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                        },
-                        {
-                            title: "Strictly Necessary Cookies <span class=\"pm__badge\">Always Enabled</span>",
-                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                            linkedCategory: "necessary"
-                        },
-                        {
-                            title: "Analytics Cookies",
-                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                            linkedCategory: "analytics"
-                        },
-                        {
-                            title: "More information",
-                            description: "For any query in relation to my policy on cookies and your choices, please <a class=\"cc__link\" href=\"#yourdomain.com\">contact me</a>."
-                        }
-                    ]
-                }
-            }
-        }
+      }
     }
+  }
 });
