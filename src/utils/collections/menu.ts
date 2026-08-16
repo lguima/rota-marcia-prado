d522caaf67041b0d118b726d547f497bc33fda3b
Lucas Guima <lucasguima.felipe@gmail.com>
2026-08-16 13:41:41 -0300

    feat(utils/collections/menu): update menu utils to handle levels

--- src/utils/collections/menu.ts
+++ src/utils/collections/menu.ts
@@ -1,5 +1,9 @@
 import { getCollection } from 'astro:content'
 
-export async function getMenuItems() {
-  return (await getCollection('menu')).sort((a, b) => a.data.order - b.data.order)
+export async function getMenuItems(levels: number[]) {
+  return (
+    await getCollection('menu', ({ data }) => {
+      return data.levels.some(level => levels.includes(level))
+    })
+  ).sort((a, b) => a.data.order - b.data.order)
 }
