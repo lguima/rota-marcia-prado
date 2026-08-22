9a30c5a1214fa0b606f3662600a49c02195148fd
Lucas Guima <lucasguima.felipe@gmail.com>
2026-08-22 11:31:13 -0300

    chore(config/analytics): remove unused property

--- src/config/analytics.mjs
+++ src/config/analytics.mjs
@@ -5,6 +5,5 @@ export default {
   googleTag: {
     enabled: import.meta.env.ANALYTICS_GOOGLE_TAG_ENABLED !== "false",
     measurementId: import.meta.env.ANALYTICS_GOOGLE_TAG_MEASUREMENT_ID ?? "G-X8970LK9KF",
-    mode: import.meta.env.ANALYTICS_GOOGLE_TAG_MODE ?? "anonymous",
   },
 }
